e65da24c2000bd05135291bf2325db303bba0f92
dunkelheitEXE <alangmoreno02@gmail.com>
2023-11-12 19:10:44 -0600

    Now, Its possible upload images

--- profile-config.php
+++ profile-config.php
@@ -6,8 +6,23 @@ if(!isset($_SESSION['userid'])) {
     header("Location: index.php");
 } else {
     //code...
+    $currentSession = $connection->prepare("SELECT profilephoto FROM users WHERE id=:id");
+    $currentSession->bindParam(':id', $_SESSION['userid']);
+    $currentSession->execute();
+    $results = $currentSession->fetch(PDO::FETCH_ASSOC);
+
+    $profilephoto = $results['profilephoto'];//STABLISH NEW PROFILE PHOTO
     if(isset($_POST['submit'])){
-        
+        $mysql = "UPDATE users SET profilephoto = :photo WHERE id=:id";
+        $req = $connection->prepare($mysql);
+        $req->bindParam(':id', $_SESSION['userid']);
+
+        //SAVE IMAGE AND UPLOAD IT TO DB
+        $photofile = "img/".$_FILES['profilephoto']['name'];
+        move_uploaded_file($_FILES['profilephoto']['tmp_name'], $photofile);
+        $req->bindParam(':photo', $photofile);
+
+        $req->execute();
     }
 }
 ?>
